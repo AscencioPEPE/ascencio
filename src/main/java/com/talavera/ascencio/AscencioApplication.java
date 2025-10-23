a7bc794a9736a7992a3ca6a122f8d15c39559087
ASCENCIO\pepej <jose.ascencio@stukent.com>
2025-10-23 16:19:49 -0500

    First commit

---
+++ src/main/java/com/talavera/ascencio/AscencioApplication.java
@@ -0,0 +1,13 @@
+package com.talavera.ascencio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscencioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscencioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talavera/ascencio/AscencioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talavera.ascencio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscencioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
